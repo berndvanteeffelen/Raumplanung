55d3716144124dcb28564644bfe89f636113bd35
Bernd van Teeffelen <berndvanteeffelen@hotmail.de>
2022-09-06 16:55:05 +0200

    Springboot Initialisierung
    Nutzung des Spring Intitalizr

---
+++ src/main/java/com/Raumplanung/RaumplanungApplication.java
@@ -0,0 +1,13 @@
+package com.Raumplanung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaumplanungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaumplanungApplication.class, args);
+	}
+
+}
